5f2ed1f9942d2e048d090756d2a27cbe67940ff5
NataliaPopova127 <pnatala127@mail.ru>
2023-05-18 08:44:29 +0300

    update api in region

--- project-etno-front-main/src/app/regions/regions.component.ts
+++ project-etno-front-main/src/app/regions/regions.component.ts
@@ -1,22 +1,52 @@
 import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
-import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbCarousel,
+  NgbSlideEvent,
+  NgbSlideEventSource,
+} from '@ng-bootstrap/ng-bootstrap';
+import { HttpService } from '../http.service';
 
 @Component({
-	selector: 'app-regions',
-	templateUrl: './regions.component.html',
-	styleUrls: ['./regions.component.css'],
-	encapsulation: ViewEncapsulation.None
+  selector: 'app-regions',
+  templateUrl: './regions.component.html',
+  styleUrls: ['./regions.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [HttpService],
 })
 export class RegionsComponent {
-	testRegionsRibbon = [
-		{ "id": "1", "reg": "Республика Адыгея", "searchName": "Адыгея", "previewPath": "/assets/adygea_preview.jpg" },
-		{ "id": "2", "reg": "Краснодарский край", "searchName": "Краснодарский край", "previewPath": "/assets/kr_krai_preview.jpg" }	
-	];
+  testRegionsRibbon = [
+    {
+      id: '1',
+      reg: 'Республика Адыгея',
+      searchName: 'Адыгея',
+      previewPath: '/assets/adygea_preview.jpg',
+    },
+    {
+      id: '2',
+      reg: 'Краснодарский край',
+      searchName: 'Краснодарский край',
+      previewPath: '/assets/kr_krai_preview.jpg',
+    },
+  ];
 
-	testRegions = this.testRegionsRibbon
+  constructor(private httpService: HttpService) {}
+  testRegions = this.testRegionsRibbon;
 
-	onSearchChange(event: any) {
-		this.testRegions = this.testRegionsRibbon.filter(t => t.reg.toLowerCase().indexOf((event.target.value as string).toLowerCase()) > -1)
-	}
+  response: any;
+  ROOT_URL = '';
+
+  ngOnInit() {
+    this.ROOT_URL = `http://localhost:8080/regions`;
+    this.httpService
+      .getData(this.ROOT_URL)
+      .subscribe((response) => (this.response = response));
+  }
+  onSearchChange(event: any) {
+    this.testRegions = this.testRegionsRibbon.filter(
+      (t) =>
+        t.reg
+          .toLowerCase()
+          .indexOf((event.target.value as string).toLowerCase()) > -1
+    );
+  }
 }
-
